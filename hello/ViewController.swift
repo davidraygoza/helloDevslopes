1ad65c680785f5cfda4620851b38b81168532122
David Raygoza <david.raygoza.ramirez@gmail.com>
2017-06-20 08:39:06 -0500

    First exercise devslopes course

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnWelcome: UIButton!
+    @IBOutlet weak var titleMessage: UIImageView!
+    @IBOutlet weak var background: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func welcomePressed(_ sender: Any) {
+        titleMessage.isHidden = false
+        background.isHidden = false
+        btnWelcome.isHidden = true
+        
+    }
+    
 }
 
